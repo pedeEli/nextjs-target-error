8a4b7cfffba57d7d90d02cfd372c03b214d9cea5
pedeEli <74102959+pedeEli@users.noreply.github.com>
2023-03-29 04:09:22 +0200

    more cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
